787166190a3ee6f477d43f16d0adfd8c6de00e0a
deepanjan06 <deepanjan.paul@hotmail.com>
2016-04-07 18:17:52 +0100

    Java program to calculate the area perimeter of a square.

---
+++ src/SquareCalculation.java
@@ -0,0 +1,17 @@
+
+public class SquareCalculation {
+	public static void main (String[] args)
+	   {
+	      int sides = 7;
+	      System.out.println ("The length of the sqaure's side is " + sides);
+ 
+	      System.out.println ("The perimeter of the sqaure is " + 4 * sides);
+
+	      System.out.println ("The area of the sqaure is " + (sides*sides));
+
+	   
+	   }
+	
+	
+	
+}
